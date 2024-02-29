c02fb8617c7eed32bda9ecb3fc50c2d743bf02f8
SeungGun <saverrw2864@naver.com>
2024-02-29 21:26:07 +0900

    chore: STOMP 웹소켓 Configuration 추가

---
+++ src/main/java/com/example/chatting/config/StompWebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.example.chatting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); //
+//        registry.setErrorHandler(); -> 에러 핸들러: 소켓통신 중 예외 발생 시 해당 핸들러로 제어권이 넘어감
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher(".")); // "/" 대신 경로를 "."으로 표현 (AMQP 관례)
+        registry.setApplicationDestinationPrefixes("/pub"); // Application Controller에서 처리하기 위한 요청 prefix
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors();
+    }
+}
