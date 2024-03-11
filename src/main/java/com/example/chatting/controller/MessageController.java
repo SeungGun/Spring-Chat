a57cd3c5aeb7548e47b71150987da953bf206c42
SeungGun <saverrw2864@naver.com>
2024-03-11 22:13:22 +0900

    refactor: 채팅방 입장, 퇴장 시 메시지 내용 수정

--- src/main/java/com/example/chatting/controller/MessageController.java
+++ src/main/java/com/example/chatting/controller/MessageController.java
@@ -1,6 +1,5 @@
 package com.example.chatting.controller;
 
-
 import com.example.chatting.dto.MessageCreateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,19 +13,23 @@ import org.springframework.stereotype.Controller;
 public class MessageController {
     private final RabbitTemplate rabbitTemplate;
 
+    // 채팅방 입장
     @MessageMapping("chat.enter.{roomId}")
-    public void enterUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto){
-
+    public void enterUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto) {
+        messageCreateDto.setContent(messageCreateDto.getSender() + "님이 입장하셨습니다.");
         rabbitTemplate.convertAndSend("chat.exchange", "enter.room." + roomId, messageCreateDto);
     }
 
+    // 채팅 + 채팅방 입장 + 채팅방 퇴장
     @MessageMapping("chat.talk.{roomId}")
-    public void talkUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto){
+    public void talkUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto) {
         rabbitTemplate.convertAndSend("chat.exchange", "*.room." + roomId, messageCreateDto);
     }
 
+    // 채팅방 퇴장
     @MessageMapping("chat.exit.{roomId}")
-    public void exitUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto){
+    public void exitUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateDto messageCreateDto) {
+        messageCreateDto.setContent(messageCreateDto.getSender() + "님이 퇴장하셨습니다.");
         rabbitTemplate.convertAndSend("chat.exchange", "exit.room." + roomId, messageCreateDto);
     }
 }
